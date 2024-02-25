fe248c8406790a5d4fbe8aac7a48ff3f9b682652
Sevann Radhak Triztan <sevann.radhak@gmail.com>
2024-02-25 16:57:55 -0300

    Add unique Index to Badge

--- src/ExampleApp.Api/01-data-example.sql
+++ src/ExampleApp.Api/01-data-example.sql
@@ -1,19 +1,37 @@
 ﻿INSERT INTO Semesters (Id, Description, StartDate, EndDate)
 VALUES 
-	(1, 'Fall 2023', '2023-03-01', '2023-09-01'), 
-	(2, 'Spring 2023', '2023-09-01', '2023-03-01');
+	(1, 'Spring 2023', '2023-01-10', '2023-04-25'), 
+	(2, 'Fall 2023', '2023-08-25', '2023-12-10'), 
+	(3, 'Spring 2024', '2024-01-15', '2024-05-05'), 
+	(4, 'Fall 2024', '2024-09-01', '2024-12-15');
 
 INSERT INTO Professors (FullName, Extension)
-VALUES ('John Smith', 'prof1'), ('Jane Smith', 'prof2');
+VALUES
+	('John Smith', 'prof1'),
+	('Mary Lu', 'prof2');
 
 INSERT INTO Courses (Id, Description, SemesterId, ProfessorId, CreatedOn, LastModifiedOn)
-VALUES ('course1', 'Math', 1, 1, GETDATE(), GETDATE()), ('course2', 'English', 2, 2, GETDATE(), GETDATE());
+VALUES
+	('course1', 'Math', 1, 1, GETDATE(), GETDATE()),
+	('course2', 'English', 2, 2, GETDATE(), GETDATE()),
+	('course3', 'Spanish', 3, 1, GETDATE(), GETDATE()),
+	('course4', 'French', 3, 2, GETDATE(), GETDATE());
 
 INSERT INTO Students (FullName, Badge, ResidentStatus)
-VALUES ('John Doe', '123', 1), ('Jane Doe', '456', 2);
+VALUES
+	('Sevann Radhak', 'A123', 1),
+	('Dylan Rametta', 'A456', 2),
+	('Melman Arigato', 'B001', 3),
+	('Marty Pelagato', 'B002', 3);
 
-DECLARE @StudentId INT;
-SELECT @StudentId = Id FROM Students WHERE FullName = 'John Doe';
+DECLARE @StudentId1 INT;
+DECLARE @StudentId2 INT;
+SELECT @StudentId1 = Id FROM Students WHERE FullName = 'Sevann Radhak';
+SELECT @StudentId2 = Id FROM Students WHERE FullName = 'Melman Arigato';
+
 
 INSERT INTO StudentCourses (StudentId, CourseId)
-VALUES (@StudentId, 'course1'), (@StudentId, 'course2');
+VALUES
+	(@StudentId1, 'course3'),
+	(@StudentId1, 'course4'),
+	(@StudentId2, 'course3');
--- src/ExampleApp.Api/Migrations/20240225193752_AddUniqueIndexToBadge.Designer.cs
+++ src/ExampleApp.Api/Migrations/20240225193752_AddUniqueIndexToBadge.Designer.cs
@@ -4,6 +4,7 @@ using ExampleApp.Api.Domain.Academia;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExampleApp.Api.Migrations
 {
     [DbContext(typeof(AcademiaDbContext))]
-    partial class AcademiaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240225193752_AddUniqueIndexToBadge")]
+    partial class AddUniqueIndexToBadge
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
