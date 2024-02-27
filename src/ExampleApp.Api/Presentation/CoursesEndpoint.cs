3d060ac633813626d39943fd714dd97c4fc182d8
marcioschlichting <marcioosergioo2015@gmail.com>
2024-02-27 20:33:17 -0300

    feat(task-2): adding correct model to the course endpoints.

--- src/ExampleApp.Api/Presentation/CoursesEndpoint.cs
+++ src/ExampleApp.Api/Presentation/CoursesEndpoint.cs
@@ -1,4 +1,5 @@
 using ExampleApp.Api.Controllers.Models;
+using ExampleApp.Api.Domain.Academia.Commands;
 using ExampleApp.Api.Domain.Academia.Queries.Students;
 using ExampleApp.Api.Extensions;
 using MediatR;
@@ -10,7 +11,13 @@ public class CoursesEndpoint
 {
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
-        app.MapPost("/courses/{courseId}", static ([FromBody] SubscribeStudentToCourseModel model, [FromServices] IMediator mediator)
+        app.MapPost("/courses/{courseId}", static ([FromBody] StudentToCourseModel model, [FromServices] IMediator mediator)
+            => mediator.SendCommand(new SubscribeStudentToCourseCommand(model)))
+            .Produces<Unit>()
+            .WithDisplayName("Associate Student to Course")
+            .WithTags("Courses");
+
+        app.MapPut("/courses/{courseId}", static ([FromBody] StudentToCourseModel model, [FromServices] IMediator mediator)
             => mediator.SendCommand(new GetStudentCoursesByCurrentSemesterQuery()))
             .Produces<Unit>()
             .WithDisplayName("Associate Student to Course")
