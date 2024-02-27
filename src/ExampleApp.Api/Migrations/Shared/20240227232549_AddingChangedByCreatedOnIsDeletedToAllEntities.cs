d22e68b198fe5783f4478ccec0951f611abb65c6
marcioschlichting <marcioosergioo2015@gmail.com>
2024-02-27 20:54:37 -0300

    feat(task-3): adding migration to the ChangedOn and CreatedOn.

---
+++ src/ExampleApp.Api/Migrations/Shared/20240227232549_AddingChangedByCreatedOnIsDeletedToAllEntities.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExampleApp.Api.Migrations.Shared
+{
+    /// <inheritdoc />
+    public partial class AddingChangedByCreatedOnIsDeletedToAllEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                table: "StudentCourses");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedOn",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChangedOn",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Students",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "StudentCourses",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChangedOn",
+                table: "StudentCourses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "StudentCourses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChangedOn",
+                table: "Semesters",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Semesters",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Semesters",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChangedOn",
+                table: "Professors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Professors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Professors",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChangedOn",
+                table: "Courses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Courses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangedOn",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "ChangedOn",
+                table: "StudentCourses");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "StudentCourses");
+
+            migrationBuilder.DropColumn(
+                name: "ChangedOn",
+                table: "Semesters");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Semesters");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Semesters");
+
+            migrationBuilder.DropColumn(
+                name: "ChangedOn",
+                table: "Professors");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Professors");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Professors");
+
+            migrationBuilder.DropColumn(
+                name: "ChangedOn",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "StudentCourses",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastModifiedOn",
+                table: "StudentCourses",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedOn",
+                table: "Courses",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastModifiedOn",
+                table: "Courses",
+                type: "datetimeoffset",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+    }
+}
